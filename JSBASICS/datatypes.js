6a1f7bc0b4ff336ac5ea81443776eff4907a8ba5
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-29 20:04:31 +0530

    Data Types in JS | Primitives and Non Primitives

---
+++ JSBASICS/datatypes.js
@@ -0,0 +1,42 @@
+/*What all values we can store in JS?
+
+-->Refer ECMAScript Docs
+
+1)-->Number:10,-3,2.6,100,1000,3,414,etc.
+2)-->String:"Sanket"        -->Double Quotes(" ")
+            'Randome Text'  -->Single Quotes(' ')
+            `Some Text`     -->Backtick(` `)
+3)-->Boolean:True or False(1 or 0)
+4)-->Undefined:It is a KEYWORD,Something not defined yet but may be defined LATER.
+5)-->Objects:If we  have to somehow store key-value pairs,then we use objects.
+{
+    name:"Sanket"
+    company:"Google"
+    positon:"Software Engineer"
+
+}
+Keys should be unique.
+We can show complex entities sing key value pairs-->that is-->OBJECT.
+
+There are 2 Types of Data Types:
+
+1)Primitive:
+Types which are atomic in natures.
+eg-Number
+
+2)Non-Primitive:
+Types which are a composition of other types.
+eg-Object
+
+
+user=
+{
+    name:"Sanket"
+    age:24
+    posts:(-->Complex entity)
+    {
+        createdAt:"Jun 12,2022"
+        text:"My first post"
+    }
+}
+*/
