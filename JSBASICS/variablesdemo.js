9820d8adc728daacc1bad23b3f52f910923a3f91
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-29 20:06:57 +0530

    Coding Variables in JS

---
+++ JSBASICS/variablesdemo.js
@@ -0,0 +1,32 @@
+//How to Create Variables in JS??
+
+// 3 Ways to Create variables-->
+
+
+// 1)Using VAR keyword
+// Syntax:-->var <variablename>= <value>
+var score=10;
+var marks=90;
+
+//2)Using LET keyword
+//Syntax:-->let <variablename>= <value> 
+let age=24;
+let flag=0;
+
+//3)Using CONST keyword
+//Syntax:-->const <variablename>= <value>
+const x=100; 
+const y=30;
+
+//Semi-Colon:
+//One line of JS Code == One instruction that we want to give it to the computer.
+//One complete instruction is called STATEMENT.
+//At the end of each statement,There should be a semi-colon.
+//Recommended to use(like a punctuation),NOT MANDATORY.
+
+//Naming Rules for Variables Creation
+//1)Variable can contain small aplhabets,capital alphabets,digits,underscore(_),dollar($).
+//2)We can't have space or any other special character.
+//3)We should give meaningful names so that reader can identify it easily.
+//4)Variable name cannot start with a digit but it can have digits in between or at last.
+//5)We cannot use keywords as variable names.
