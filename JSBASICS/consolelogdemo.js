aadb6f6603fd758094878866908d924ec0a07774
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-29 20:03:08 +0530

    Showing Data to User using console log

---
+++ JSBASICS/consolelogdemo.js
@@ -0,0 +1,18 @@
+/*Functions as a black box.
+-->takes Some Input
+-->Gives some Output
+
+In-built Functions
+1)console.log --->this function takes some data as input and then displays them int hte output.
+
+-->If we have 2 different console.log(),then both of them display.print poutput in different ways.
+-->we can also pass multiple values together to console.log and all of them will be on the same line.(using Comma-CSV-Comma Separated Values)
+
+*/
+console.log(10);
+console.log("Hello");
+
+let age=13;
+console.log(age);
+
+console.log(age,342,"Random text",45);
