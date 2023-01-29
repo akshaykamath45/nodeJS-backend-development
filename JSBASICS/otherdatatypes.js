a37727e6b8762301041780de96b67d24158577f2
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-01-29 20:05:25 +0530

    Other Datatypes

---
+++ JSBASICS/otherdatatypes.js
@@ -0,0 +1,25 @@
+/*
+Continued from previous Datatypes
+
+6)Null:It actually represents empty value
+
+*/
+var marks=100;
+var name="Sanket";
+var age;//Undefined
+var company=null;
+var salary=undefined;
+var isStudent=false;
+console.log("Marks = ",marks);
+console.log("Name = ",name);
+console.log("Age = ",age);//Undefined
+console.log("Company = ",company);
+console.log("Salary = ",salary);
+console.log("Is a Student ? = ",isStudent);
+//For complex entities,we use OBJECTS>
+var user={
+    name:"Sanket",
+    company:"Google",
+    age:24
+}
+console.log("Details of the user is ",user);
