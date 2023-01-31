1b2cbf2737cf6e16652a5d9b4f2a030297dc50ba
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-01 03:59:32 +0530

    Learning ToBoolean
    Referred the following links
    
    ToBoolean : https://262.ecma-international.org/10.0/#sec-toboolean
    
    Undefined→False
    Null→False
    For Number →if it is +0,-0 or NaN,it will return false,otherwise return true for all other values.
    For String→If it is an empty string,it will return false,otherwise return true for all other values.
    
    “Undefined,Null,NaN,+0,-0,false”-->These all are falsy values

---
+++ INTROCUCTION TO COERCION AND ABSTRACT OPERATORS/ToBoolean.js
@@ -0,0 +1,13 @@
+let x=10;
+console.log(!x);
+
+let y=undefined;
+console.log(!y);
+
+//We can use it in CONDITIONAL STATEMENTS.
+
+if(y){
+    console.log("It is Truthy");
+}else{
+    console.log("It is Falsy");
+}
