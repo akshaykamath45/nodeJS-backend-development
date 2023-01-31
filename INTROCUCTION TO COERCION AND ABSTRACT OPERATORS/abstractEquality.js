d7f20eb5e7e1de90736ab23ee69b09a7a3cf1614
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-01 04:20:25 +0530

    Learning Abstract Equality
    Referred the following links :
    
    Abstract Equality : https://262.ecma-international.org/10.0/#sec-abstract-equality-comparison
    
    If x→Number,y→String→Converts x→ToNumber and then again does the  comparison.
    If x→String,y→Number→Converts x→ToNumber and then again does the comparison.
    If x→Boolean→Convert x→ToNumber and then again does the comparison.
    If y→Boolean→Convert y→ToNumber and then again does the comparison.

---
+++ INTROCUCTION TO COERCION AND ABSTRACT OPERATORS/abstractEquality.js
@@ -0,0 +1,14 @@
+console.log(null == undefined);//True
+
+console.log(12 == "12");//True
+
+console.log(false == "0");//True
+// if  x→Boolean→ToNumber→false→0
+// x=0,y=”0”,x==y
+// y→ToNumber→0
+// 0==0—>true
+
+console.log(null == "false");//False
+//y-->Boolean-->ToNumber-->0
+//null == 0
+//False-->Satisfying the last conditon.
