638a8713d8f9e7d00918a090e7957dbd109a46d3
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-04 15:56:28 +0530

    Learning Strict Equality
    Referred the following links :
    
    Strict Equality :  https://262.ecma-international.org/10.0/#sec-strict-equality-comparison
    
    SameValueNonNumber : https://262.ecma-international.org/10.0/#sec-samevaluenonnumber
    
    If x or y is NaN,returns false.
    If x=y,returns true.
    If x=+0 and y=-0,returns true.
    If x=-0 and y=+0,returns true.
    Else returns false.
    
    Type of Null is →Object→Mistake.
    
    When You compare same memory objects,it will return true,when you compare different memory objects,it will return false.

--- INTROCUCTION TO COERCION AND ABSTRACT OPERATORS/strictEquality.js
+++ INTROCUCTION TO COERCION AND ABSTRACT OPERATORS/strictEquality.js
@@ -1,2 +1,11 @@
 console.log(NaN===NaN);//False
 console.log(0===-0);//True
+
+let obj1={x:10};
+let obj2={y:10};
+let obj3={z:10};
+
+console.log(obj1===obj2);//False
+console.log(obj1===obj3);//False
+console.log(obj1===obj1);//True
+console.log({x : 10}==={x:10});//False-->Created at 2 different locations in the memory.
