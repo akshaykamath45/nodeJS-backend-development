c2695287344f2a39b49a5a2bfd6d7f055b0f81a1
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-04 17:30:31 +0530

    Boxing in JavaScript
    Referred the following links :
    
    Boxing : https://javascript.plainenglish.io/javascript-boxing-wrappers-5b5ff9e5f6ab
    
    Primitive Types gets Converted to Objects-->Kind Of Explicit Conversion
    
    Boxing-->Wrapping a Primitve Value in the Object.

---
+++ INTROCUCTION TO COERCION AND ABSTRACT OPERATORS/boxing.js
@@ -0,0 +1,11 @@
+//Primitive Types gets Converted to Objects-->Kind Of Explicit Conversion
+(1).toString();//"1"
+Number(1).toString();//"1"
+x=10;
+console.log(x.toString());//"10"
+console.log((10).toString())//10
+
+//String primitive name
+const name = "KESK";
+name.toLowerCase();//kesk
+name.substring(1);//ESK
