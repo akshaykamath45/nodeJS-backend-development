94b0d7bce168955129bcaf1d32805b05a205de50
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-02-04 17:03:33 +0530

    Learning NaN Number
    Referred the following links :
    
    Number.isNaN : https://262.ecma-international.org/10.0/#sec-number.isnan
    
    It converts the incoming input to a number by performing ToNumber Operation.
    
    Abstract(==) and Strict Equality(===)  is not the ideal case to check or compare NaN values.

--- INTROCUCTION TO COERCION AND ABSTRACT OPERATORS/NaN.js
+++ INTROCUCTION TO COERCION AND ABSTRACT OPERATORS/NaN.js
@@ -1,21 +1,21 @@
-console.log(Number("123"));
-console.log(Number("abcd"));
-console.log(Number("0xa"));
+console.log(Number("123"));//123
+console.log(Number("abcd"));//NaN
+console.log(Number("0xa"));//10
 
 let x = NaN;
 
-console.log(x == NaN);
+console.log(x == NaN);//false
 
-console.log(isNaN(x));
+console.log(isNaN(x));//true
 
 console.log(isNaN("sanket")); // isNaN converts the incoming input to a number-->Performs ToNumber conversion
 
-console.log(Number.isNaN("sanket"));
-console.log(Number.isNaN(x));
+console.log(Number.isNaN("sanket"));//false
+console.log(Number.isNaN(x));//true
 
 if(typeof(x) == 'number' && x !== x ) {
-    console.log(true);
+    console.log(true);//true
 }
 
-console.log(x !== x); // ! == is there any problem with this expression to check nan value
+console.log(x !== x); // ! == is there any problem with this expression to check nan value --> true
 // Why is this true?Because x is NaN and NaN is not equal to NaN    
