fb21ecb10994939ccfd74ddad82ef795d86f8a88
Akshay Kamath <akshay.kamath054@nmims.edu.in>
2023-03-18 19:53:53 +0530

    Q5
    Make a fake fetch call that takes a message and a boolean value to get data and console the message received from the server. A fake fetch has been provided. You can practice this question in any JS editor or your browser console.

---
+++ ASYNC/Practice Questions/5.js
@@ -0,0 +1,16 @@
+const fakeFetch = (msg, shouldSucceed) => {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (shouldSucceed) {
+          resolve(`message from server: ${msg}`);
+        }
+        reject(`error from server: ${msg}`);
+      }, 3000);
+    });
+  };
+  
+  fakeFetch("Hi",true)
+  .then((data)=>console.log(data))
+  .catch((err)=>console.error(err));
+  
+  // Hi -- after 3 seconds
